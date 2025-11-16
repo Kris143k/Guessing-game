81359b56132c7b74c3d8054fe8423a7b0ce735ce
Krish Patel <130545320+Kris143k@users.noreply.github.com>
2025-11-16 19:30:46 +0530

    Convert GuessingGame from Java to C#

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,32 +1,37 @@
-import java.util.Random;
-import java.util.Scanner;
+using System;
 
-public class GuessingGame {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+public class GuessingGame
+{
+    public static void Main(string[] args)
+    {
         Random random = new Random();
+        int randomNumber = random.Next(1, 101); // 1 to 100
+        int guess = 0;
+        int attempts = 0;
 
-        int randomNumber = random.nextInt(100)+1;
-        int guess=0;
-        int attempts=0;
+        Console.WriteLine("Welcome to the Guessing Game!");
+        Console.WriteLine("I have selected a number between 1 and 100");
+        Console.WriteLine("Try to guess it!");
 
-        System.out.println("Welcome to the Guessing Game!");
-        System.out.println("I have selected a number between 1 and 100");
-        System.out.println("Try to guess it!");
-
-        while(guess!=randomNumber){
-            System.out.println("Enter your guess: ");
-            guess=sc.nextInt();
+        while (guess != randomNumber)
+        {
+            Console.Write("Enter your guess: ");
+            guess = Convert.ToInt32(Console.ReadLine());
             attempts++;
-            if(guess<randomNumber){
-                System.out.println("Your guess id too low.");
-            }else if(guess>randomNumber){
-                System.out.println("Your guess is too high.");
-            }else{
-                System.out.println("Congratulations! You have guessed the correct number.");
-                System.out.println("It took you "+attempts+" attempts.");
+
+            if (guess < randomNumber)
+            {
+                Console.WriteLine("Your guess is too low.");
+            }
+            else if (guess > randomNumber)
+            {
+                Console.WriteLine("Your guess is too high.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You have guessed the correct number.");
+                Console.WriteLine("It took you " + attempts + " attempts.");
             }
         }
-        sc.close();
     }
 }
